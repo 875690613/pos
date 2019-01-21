a5270cb304d3ddb26687a74db2a1a0b1b78dd58e
875690613 <875690613@qq.com>
2019-01-21 21:56:04 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <leftNav></leftNav>
+    <left-nav></left-nav>
+    <div class="main">
+
+      <router-view/>
+    </div>
+  </div>
+</template>
+<script>
+import leftNavigation from '@/components/common/leftNav'
+export default {
+  name: 'App',
+  components: {
+    leftNav: leftNavigation
+  }
+}
+</script>
+
+<style>
+body{margin: 0; height: 100%;}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  overflow: hidden;
+  /* margin-top: 60px; */
+}
+.main{
+  float: left;overflow: hidden; width: 95%; height:100%; background: #eff2f7;
+}
+</style>
---
+++ src/components/page/Pos.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+    </div>
+</template>
+
+<script>
+const a = 0;
+
+export default {
+  name: 'Pos'
+}
+</script>
+
+<style scoped>
+</style>
